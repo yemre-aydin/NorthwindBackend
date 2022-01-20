badc5413ee823f00bddda8203a616a3a7df988fe
veli akgün <aliveli_ben@hotmail.com>
2022-01-20 20:07:02 +0300

    master 20.01

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,13 @@ namespace Business.Abstract
 {
     interface IProductService
     {
+        //tüm listeyi getiren bir operasyon yazılacak
+        Product GetById(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+
     }
 }
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        //Dependency Injection Altyapısı
+        private IProductDal _productDal;
+        
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public void Add(Product product)
+        {
+           // Business codes kurallar buraya yazılıyor
+
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public Product GetById(int productId)
+        {
+            
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
