8d48139edb55c26a7637663a01d96c24272fdb55
veli akgün <aliveli_ben@hotmail.com>
2022-01-20 15:49:48 +0300

    Proje dosyası ekle.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,64 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{//operasyonlar
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+    where TEntity : class, IEntity, new()
+    where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (var context=new TContext())//eklemek için 
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context = new TContext())
+            {//get operasyonu abone olma operasyonu select sorgularını yazıyoruz.
+                //verdiğimiz filtreye göre o datanın gelmesi burada sağlanıyor
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public IList<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {//filtre gönderilmemişse tamamını getirmesini sağlıyor
+            using (var context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList(); 
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace DataAccess.Abstract
+    //veriye erişim sağlayacağımız dataaccess katmanı
+
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+    }
+}
